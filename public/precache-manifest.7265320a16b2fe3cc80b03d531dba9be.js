492a642f51d0aa895bc1f7d2a22ba47640adb1d2
Joshua Taguicana <joshtaguicana@gmail.com>
2020-06-24 18:02:20 -0400

    completed front end for app

---
+++ public/precache-manifest.7265320a16b2fe3cc80b03d531dba9be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e7fe933b100e02b19219a2f9a5e3576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7470f986dacc7e084d53",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4e14ae18753112ca445a",
+    "url": "/static/js/2.c8e1aa37.chunk.js"
+  },
+  {
+    "revision": "6358e3a1edeb3407c864ac0519a42d91",
+    "url": "/static/js/2.c8e1aa37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7470f986dacc7e084d53",
+    "url": "/static/js/main.fec63021.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
